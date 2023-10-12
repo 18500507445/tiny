20340a18b5b0186b4ca641221bdf00c230c7ecb7
wzh <18500507445@163.com>
2023-10-12 17:54:11 +0800

    update：初始化ip修改

--- tiny-example/src/main/java/com/tiny/example/common/CommonInit.java
+++ tiny-example/src/main/java/com/tiny/example/common/CommonInit.java
@@ -1,4 +1,4 @@
-package com.tiny.order.common;
+package com.tiny.example.common;
 
 import com.tiny.common.core.result.RespResult;
 import com.tiny.common.core.utils.common.IpUtils;
@@ -9,19 +9,23 @@ import javax.annotation.PostConstruct;
 
 /**
  * @author: wzh
- * @description 当前机器ip初始化
+ * @description 通用初始化类
  * @date: 2023/10/12 11:41
  */
 @Component
 @Slf4j(topic = "IpInit")
-public class IpInit {
+public class CommonInit {
 
     public static String INTERNET_IP = "00";
 
+    /**
+     * 初始化ip
+     */
     @PostConstruct
-    void init() {
+    void initIp() {
         INTERNET_IP = IpUtils.getInternetIp("curl cip.cc");
-        log.warn("获取公网ip初始化后几位完成");
+        log.warn("获取公网ip后2位，初始化RespResult.IP");
         RespResult.IP = INTERNET_IP;
     }
+
 }
