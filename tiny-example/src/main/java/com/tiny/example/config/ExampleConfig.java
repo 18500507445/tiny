970bc18a53e1c26dade591ede40fe2b714479d5f
wzh <18500507445@163.com>
2024-03-27 10:52:12 +0800

    add：分页补充

--- tiny-example/src/main/java/com/tiny/example/config/ExampleConfig.java
+++ tiny-example/src/main/java/com/tiny/example/config/ExampleConfig.java
@@ -1,13 +1,20 @@
 package com.tiny.example.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author: wzh
- * @description: 样例配置类
+ * @description: 样例配置类，两种方式（1.@Value注解获取  2.@ConfigurationProperties(prefix = "spring.data")通配整个类）
  * @date: 2023/11/29 15:33
  */
+@Data
 @Configuration
 public class ExampleConfig {
+
+    //给个默认值，万一线上没配置
+    @Value("${spring.data.example:a}")
+    private String a;
 
 }
